af26c0178dbd6640df4a3d2e5de47699300c1aed
Ahir Roy <ahara32@gmail.com>
2019-11-01 02:03:59 -0400

    Update Canada_bank_stock.py

--- Canada_bank_stock.py
+++ Canada_bank_stock.py
@@ -26,14 +26,12 @@ cf.go_offline()
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[2]:
 
 
 start = datetime.datetime(2011, 1, 1)
 end = datetime.datetime(2016, 1, 1)
 
 
-# In[4]:
 
 
 #Bank of Montreal
@@ -52,32 +50,26 @@ RY = data.DataReader("RY", 'yahoo', start, end)
 TD = data.DataReader("TD", 'yahoo', start, end)
 
 
-# In[5]:
+
 
 
 tickers = ['BMO', 'BNS', 'CM', 'RY', 'TD'] #list of tickers
 
 
-# In[9]:
 
 
 bank_stocks = pd.concat([BMO, BNS, CM, RY, TD], axis = 1, keys= tickers)
 bank_stocks.columns.names = ['Bank Ticker Symbol', 'Stock Info']
 
 
-# In[10]:
-
 
 bank_stocks.head()
 
-
-# In[39]:
 
 
 RY[['Open','Close']].iplot(kind='spread') 
 
 
-# In[45]:
 
 
 for tick in tickers:
@@ -85,7 +77,6 @@ for tick in tickers:
 plt.legend()
 
 
-# In[43]:
 
 
 sns.heatmap(bank_stocks.xs(key='Open',axis=1,level='Stock Info').corr(),annot=True)
